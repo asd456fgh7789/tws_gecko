7da5b225ef0b602955d52a25dadb01c8df2191d1
Allegretic <asd456fgh7789ss@gmail.com>
2017-11-07 19:16:33 +0000

    fix TwsGecko::Time.now

--- lib/tws_gecko/time.rb
+++ lib/tws_gecko/time.rb
@@ -2,14 +2,14 @@ require 'tws_gecko/date'
 
 module TwsGecko::Time
   # ZONE_OFFSET: UTC+08:00(Asia/Taipei)
-  ZONE_OFFSET ||= 28_800
+  ZONE ||= '+08:00'.freeze
   OPEN ||= '09:00'.freeze
   CLOSE ||= '13:30'.freeze
 
   # String Constant of After-Hour Fixed-Price time
   AFHP ||= '14:30'.freeze
   def self.now
-    Time.now.utc + ZONE_OFFSET
+    Time.now.utc.localtime(ZONE)
   end
 
   def self.open_now?
