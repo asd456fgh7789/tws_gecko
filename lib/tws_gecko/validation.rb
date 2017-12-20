227888797503f6443ffe3c0876ae2dc88d06c2a4
Allegretic <asd456fgh7789ss@gmail.com>
2017-12-20 11:09:40 +0000

    check

--- lib/tws_gecko/validation.rb
+++ lib/tws_gecko/validation.rb
@@ -2,12 +2,15 @@ require 'tws_gecko/file'
 require 'tws_gecko/listed'
 
 module TwsGecko::Validation
+  extend TwsGecko::File
   HISDIR = TwsGecko::File::HISDIR
 
   class << self
     def history(symbol)
       return false unless d0 = TwsGecko::CompanyList.listed_date(symbol)
-      arr = CSV.read(HISDIR + symbol.to_s + '.csv')
+      filepath = HISDIR + symbol.to_s + '.csv'
+      file_check filepath
+      arr = CSV.read(filepath)
       d0 = d0 < Date.new(1992, 1, 4) ? Date.new(1992, 1, 4) : d0
       arr.uniq! {|i| i.first}
       d1 = arr.first.first.split('/').map(&:to_i)
@@ -15,6 +18,7 @@ module TwsGecko::Validation
       data_arr = []
       if(d1 > d0)
         date_arr = (d0...d1).map {|d| Date.new(d.year, d.month, 1) }.uniq
+        
         date_arr.each do |d|
           c = TwsGecko::History.new(symbol, d)
           c.monthly
@@ -23,7 +27,7 @@ module TwsGecko::Validation
         end
       end
       data_arr.flatten(1).reverse_each {|d| arr.unshift d }
-      CSV.open(HISDIR + symbol.to_s + '.csv', 'w') do |csv|
+      CSV.open(filepath, 'w') do |csv|
         arr.each {|i| csv << i}
       end
     end
